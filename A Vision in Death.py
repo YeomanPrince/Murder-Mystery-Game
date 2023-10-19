d2abe2471177eb2a1d518226a626bb7a58ecb579
nicole fleming <nikkiheatfleming@gmail.com>
2023-10-19 15:12:12 -0400

    Added intro and title!

---
+++ A Vision in Death.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    while True:
+        print("Welcome to A Vision in Death by Yeoman!")
+        print("As a newly minted homicide detective, you've been called out of your bed to your first case.")
+        print("Entering the scene you see a man face down in the living room, furniature strewn about looking like a struggle ensued.")
+        print("An officer comes up to you and says 'You're new here... What's your name?'")
+        name = input()
+        print("'+name+ ... Ah! You're the new Dectective! It's a grusome one for your first case!")
+        introScene()
